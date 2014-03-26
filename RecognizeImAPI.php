24f9844bd08aa5fcdedc0f34c61198a75f6a63a8
Marcin Szajek <marcin.szajek@itrafftech.com>
2014-03-26 14:09:51 +0100

    drawing frames on image using result data

--- RecognizeImAPI.php
+++ RecognizeImAPI.php
@@ -154,6 +154,26 @@ class RecognizeImAPIResult {
 	public function getObjects() {
 		return $this->objects;
 	}
+
+	public function drawFrames($file) {
+		$im = imagecreatefromstring($file);
+		if (!$im) return false;
+		$color = imagecolorallocate($im, 255, 255, 255);
+		foreach ($this->objects as $object) {
+			$location = $object->getLocation();
+			$size = count($location);
+			for ($i = 0; $i < $size; ++$i) {
+				$p1 = $location[$i];
+				$p2 = $location[($i+1)%$size];
+				imageline($im, $p1['x'], $p1['y'], $p2['x'], $p2['y'], $color);
+			}
+		}
+		ob_start();
+		imagejpeg($im);
+		$img = ob_get_clean();
+		imagedestroy($im);
+		return $img;
+	}
 }
 
 class RecognizeImAPIResultObject {
--- index.php
+++ index.php
@@ -10,6 +10,10 @@ if (RecognizeImAPI::checkImageLimits($imagePath, $mode)) {
 	$singleOneResult = RecognizeImAPI::recognize(file_get_contents($imagePath), $mode);
 	$singleAllResults = RecognizeImAPI::recognize(file_get_contents($imagePath), $mode, TRUE);
 	echo $singleOneResult, "\n", $singleAllResults, "\n";
+	if ($singleOneResult->isOK()) {
+		$im = $singleOneResult->drawFrames(file_get_contents($imagePath));
+		file_put_contents('frames.jpg', $im);
+	}
 } else {
 	echo "Image does not fulfill the requirements.\n";
 }
